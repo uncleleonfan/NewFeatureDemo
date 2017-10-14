85d74bf467205d4ed726b82ea969643f25c08ed8
Leon <526247082@qq.com>
2017-10-14 19:46:12 +0800

    Rmove outlineProvider.

--- app/src/main/java/com/example/leon/newfeaturedemo/ElevationAndShadowsActivity.java
+++ app/src/main/java/com/example/leon/newfeaturedemo/ElevationAndShadowsActivity.java
@@ -1,13 +1,8 @@
 package com.example.leon.newfeaturedemo;
 
-import android.graphics.Outline;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewOutlineProvider;
 
 /**
  * Created by Leon on 0010.
@@ -19,18 +14,6 @@ public class ElevationAndShadowsActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elevation_and_shadows);
-
-        View view = findViewById(R.id.view);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            view.setOutlineProvider(new ViewOutlineProvider() {
-                @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-                @Override
-                public void getOutline(View view, Outline outline) {
-                    outline.setOval(0, 0, view.getWidth(), view.getHeight());
-                }
-            });
-            view.setClipToOutline(true);
-        }
     }
 
 }
